f2969fd88aa580622e3a78584fc02c826048f73b
juan-francoa <esteban_rlt159@hotmail.com>
2022-10-12 22:05:09 -0500

    implementando-metodos-task-2

--- main.js
+++ main.js
@@ -1,125 +1,37 @@
-function cardMain(data) {
-    let conteMain = document.getElementById("main")
-    let diy = document.createElement("div")
-    diy.className += (" d-flex flex-wrap justify-content-center gap-5")
-    conteMain.appendChild(diy)
-    for (let i = 0;i<data.events.length;i++) {
-        let div = document.createElement("div")
-        div.innerHTML += 
+function imprimirCard(padre, value){
+    let div = document.createElement("div")
+    div.innerHTML = 
         `
         <div class="card" style="width: 18rem;">
-            <img src="${data.events[i]["image"]}" class="card-img-top" alt="${data.events[i]["name"]}">
+            <img src="${value["image"]}" class="card-img-top" alt="${value["name"]}">
             <div class="card-body">
-                <h5 class="card-title">${data.events[i]["name"]}</h5>
-                <p class="card-text">${data.events[i]["description"]}</p>
+                <h5 class="card-title">${value["name"]}</h5>
+                <p class="card-text">${value["description"]}</p>
                 <a href="#" class="btn btn-primary">Details</a>
             </div>
         </div>
-        `  
-        diy.appendChild(div)   
-    }
+        `
+        padre.appendChild(div)    
+}
+function cardMain(data){
+    let conteMain = document.getElementById("main")
+    conteMain.className = ("d-flex flex-wrap justify-content-center gap-5")
+    let card = data.events.map(value => imprimirCard(conteMain, value))
 }
 //upcoming
 function cardUpcoming(data) {
     let container = document.getElementById("upcoming")
-    let diy = document.createElement("div")
-    diy.className += (" d-flex flex-wrap justify-content-center gap-5")
-    container.appendChild(diy)
-    for (let i = 0; i < data.events.length; i++) {
-        if (!upcomingEvents(data, i)) {
-            let div = document.createElement("div")
-            div.innerHTML +=
-                `
-                <div class="card" style="width: 18rem;">
-                    <img src="${data.events[i]["image"]}" class="card-img-top" alt="${data.events[i]["name"]}">
-                    <div class="card-body">
-                        <h5 class="card-title">${data.events[i]["name"]}</h5>
-                        <p class="card-text">${data.events[i]["description"]}</p>
-                        <a href="#" class="btn btn-primary">Details</a>
-                    </div>
-                </div>
-                `
-            diy.appendChild(div)
-        }
-    }
+    container.className = ("d-flex flex-wrap justify-content-center gap-5")
+    let fecha = new Date(data.currentDate)
+    let card = data.events.filter(value => (fecha >= new Date(value.date) && imprimirCard(container,value)))
 }
-function upcomingEvents(data, i) {
-    for (let y = 0; y < 3; y++) {
-        if (converDate(data, i)[y] < converCurrentDate(data)[y]) {
-            return true
-        }
-    }
-    return false
-}
-function converDate(data, i) {
-    let array = []
-    let arraystring = []
-    for (let it = 0; it < data.events[i]["date"].length; it++) {
-        arraystring.push(data.events[i]["date"][it])
-        if (it === 3) {
-            array.push(Number(arraystring.join("")))
-            it++
-            arraystring = []
-        }
-        else if (it === 6) {
-            array.push(Number(arraystring.join("")))
-            it++
-            arraystring = []
-        }
-        else if (it === 9) {
-            array.push(Number(arraystring.join("")))
-            it++
-            arraystring = []
-        }
-    }
-    return array
-}
-function converCurrentDate(data) {
-    let array = []
-    let arraystring = []
-    for (let it = 0; it < data.currentDate.length; it++) {
-        arraystring.push(data.currentDate[it])
-        if (it === 3) {
-            array.push(Number(arraystring.join("")))
-            it++
-            arraystring = []
-        }
-        else if (it === 6) {
-            array.push(Number(arraystring.join("")))
-            it++
-            arraystring = []
-        }
-        else if (it === 9) {
-            array.push(Number(arraystring.join("")))
-            it++
-            arraystring = []
-        }
-    }
-    return array
-}
+
 //past
 function cardpast(data) {
     let container = document.getElementById("past")
-    let diy = document.createElement("div")
-    diy.className += (" d-flex flex-wrap justify-content-center gap-5")
-    container.appendChild(diy)
-    for (let i = 0; i < data.events.length; i++) {
-        if (upcomingEvents(data, i)) {
-            let div = document.createElement("div")
-            div.innerHTML +=
-                `
-                <div class="card" style="width: 18rem;">
-                    <img src="${data.events[i]["image"]}" class="card-img-top" alt="${data.events[i]["name"]}">
-                    <div class="card-body">
-                        <h5 class="card-title">${data.events[i]["name"]}</h5>
-                        <p class="card-text">${data.events[i]["description"]}</p>
-                        <a href="#" class="btn btn-primary">Details</a>
-                    </div>
-                </div>
-                `
-            diy.appendChild(div)
-        }
-    }
+    container.className = ("d-flex flex-wrap justify-content-center gap-5")
+    let fecha = new Date(data.currentDate)
+    let card = data.events.filter(value => (fecha < new Date(value.date) && imprimirCard(container,value)))
 }
 let Main = document.getElementById("main")
 if(Main !== null){
